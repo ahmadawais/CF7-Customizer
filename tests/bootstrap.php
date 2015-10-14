9ebabec787fc5151b499e4e11bdb1736f8a1ecc8
Ahmad Awais <mrahmadawais@gmail.com>
2015-10-14 12:00:04 +0500

    New travis.yml and phpunit tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,25 +1,23 @@
 <?php
-
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
-if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
-}
-
-define('PLUGIN_NAME','cf7-customizer.php');
-define('PLUGIN_FOLDER',basename(dirname( __DIR__ )));
-define('PLUGIN_PATH',PLUGIN_FOLDER.'/'.PLUGIN_NAME);
+/**
+ * Bootstrap the plugin unit testing environment.
+ *
+ * Edit 'active_plugins' setting below to point to your main plugin file.
+ *
+ * @package wordpress-plugin-tests
+ */
 
 // Activates this plugin in WordPress so it can be tested.
 $GLOBALS['wp_tests_options'] = array(
-  'active_plugins' => array( PLUGIN_PATH ),
+	'active_plugins' => array( 'cf7-customizer/cf7-customizer.php' ),
 );
 
-require_once $_tests_dir . '/includes/functions.php';
+// If the develop repo location is defined (as WP_DEVELOP_DIR), use that
+// location. Otherwise, we'll just assume that this plugin is installed in a
+// WordPress develop SVN checkout.
 
-function _manually_load_plugin() {
-	require dirname( __DIR__ ) . '/'.PLUGIN_NAME;
+if( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
+	require getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit/includes/bootstrap.php';
+} else {
+	require '../../../../tests/phpunit/includes/bootstrap.php';
 }
-
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
-
-require $_tests_dir . '/includes/bootstrap.php';
