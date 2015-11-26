fe8184e6922decf43934dc8333c4cd23215f4e4a
Ahmad Awais <mrahmadawais@gmail.com>
2015-11-26 05:02:21 +0500

    Freemius added.

--- assets/inc/cfc.php
+++ assets/inc/cfc.php
@@ -21,6 +21,49 @@ if ( file_exists( CFC_DIR . '/assets/admin/inc/welcome/welcome.php' ) ) {
 }
 
 
+/**
+ * Freemius SDK.
+ *
+ * Adds freemius SDK.
+ * Create a helper function for easy SDK access.
+ *
+ * @since 1.0.1
+ */
+if ( file_exists( CFC_DIR . '/assets/admin/inc/freemius/start.php' ) ) {
+
+	function cfc_fs() {
+	    global $cfc_fs;
+	    if ( ! isset( $cfc_fs ) ) {
+	        // Include Freemius SDK.
+		    require_once( CFC_DIR . '/assets/admin/inc/freemius/start.php' );
+		    $cfc_fs = fs_dynamic_init( array(
+				'id'              => '91',
+				'slug'            => 'cf7-customizer',
+				'public_key'      => 'pk_0b419183244d583e3b33a7736921c',
+				'is_live'         => true,
+				'WP_FS__DEV_MODE' => true,
+				'is_premium'      => false,
+				'has_addons'      => false,
+				'has_paid_plans'  => false,
+			    'menu'           => array(
+				    'slug'    => 'cfc_welcome_page',
+				    'account' => false,
+				    'contact' => false,
+				    'support' => false,
+				    'parent'  => array(
+					    'slug' => 'wpcf7',
+				    )
+			    ),
+		    ) );
+	    }
+	    return $cfc_fs;
+	}
+	// Init Freemius.
+	cfc_fs();
+
+}
+
+
 
 /**
  * Titan Framework
